b15bf55c66c05cdf7cb63dffea5a7325324836ce
Ashish Adhikari <2060aashishadhikari@gmail.com>
2022-12-07 20:45:26 +0545

    basic setup completed

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,13 +1,11 @@
+import path from 'path';
+
 export default ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'sqlite',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'backend'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'root'),
-      ssl: env.bool('DATABASE_SSL', true),
+      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
+    useNullAsDefault: true,
   },
 });
